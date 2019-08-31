d5035023c79d3e7688fc33b208059cd1155d05b9
MCVNick <mcvnick@gmail.com>
2019-08-31 09:33:38 -0600

    added nodejs to serve frontend

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express');
+const { json } = require('express')
+const expressStaticGzip = require('express-static-gzip')
+
+const app = express();
+const path = require('path')
+app.use(express.static(`${__dirname}/../build`))
+app.use(`/build/client`, expressStaticGzip(`/build/client`, {
+  enableBrotli: true,
+  customCompressions: [{
+    encodingName: 'deflate',
+    fileExtension: 'zz'
+  }],
+  orderPreference: ['br', 'gz']
+}));
+app.use(json())
+
+const port = 4005
+app.listen(port, console.log('The server is running on port', port))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
